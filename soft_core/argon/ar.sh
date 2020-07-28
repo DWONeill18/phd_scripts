e09b1755fc7cf3e0dac852b65456f2be05aca0dd
DWONeill18 <d.w.oneill.18@gmail.com>
2020-07-28 23:03:42 +0100

    Added ar.sh and ar_script.sh to soft_core directory

---
+++ soft_core/argon/ar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Liquid Argon" > argon.gro
+echo " 1" >> argon.gro
+echo "    1Ar      Ar    1   1.000   1.000   1.000 0.0000 0.0000 0.0000" >> argon.gro
+echo "   2.000   2.000  2.000 " >> argon.gro
+
+exit 0
+
---
+++ soft_core/argon/ar_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Liquid Argon" > argon.gro
+echo " 1" >> argon.gro
+echo "    1Ar      Ar    1   1.000   1.000   1.000 -0.1635 -0.0062 -0.0483" >> argon.gro
+echo "   2.000   2.000  2.000 " >> argon.gro
+
+exit 0
+
